a6c70956d405d3d1acf34b50cd0719b8bece0b45
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-01-14 10:36:09 +0000

    Update Apothem Calculator.php

--- Apothem Calculator.php
+++ Apothem Calculator.php
@@ -1,1 +1,12 @@
-
+<?php
+function polyapothem($n, $a)
+{
+	if ($a < 0 && $n < 0)
+		return -1;
+	return $a / (2 * tan((180 / $n) *
+					3.1416 / 180));
+}
+// Enter n and a values here
+$a = 9; $n = 6;
+echo polyapothem($n, $a) . "\n";
+?>
